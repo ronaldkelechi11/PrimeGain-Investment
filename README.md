12ba2077f7164ea116414983a9f3cf8ce8357770
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-10-14 12:03:33 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Prime Gain Investment
---
+++ js/index.js
@@ -0,0 +1,7 @@
+var sitename = document.getElementById("sitename");
+
+function reload() {
+    console.log("Trying to reload");
+    document.location.reload();
+    return false;
+}
