be2ca894f9d52a884bdde8e037dad9378f4c2f6a
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-10-26 06:28:14 +0100

    Initialized User fields

--- js/index.js
+++ js/index.js
@@ -21,6 +21,23 @@ function hideScreen() {
 }
 
 function createNewUser() {
-  
+  //Variable Colors
+  var red = "#cc3c3c";
+  var green = "#60dd60"; 
+
+  //Initialization
+  var username = document.getElementById("username");
+  var number = document.getElementById("number");
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  var re_password = document.getElementById("re_password");
+
+  // Value
+  var usernametxt = document.getElementById("username").value;
+  var numbertxt = document.getElementById("number").value;
+  var emailtxt = document.getElementById("email").value;
+  var passwordtxt = document.getElementById("password").value;
+  var re_passwordtxt = document.getElementById("re_password").value;
+
   return false;
 }
