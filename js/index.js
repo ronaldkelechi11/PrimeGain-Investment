e87c13433d2c1acd8fec718a36642137ed0bf38b
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-11-06 22:58:46 +0100

    2nd Redeployment

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
+//Button Initialization
 document.getElementById("reg").addEventListener("click", hideRegister);
 document.getElementById("log").addEventListener("click", hideLogin);
-
 document.getElementById("cancelButton").addEventListener("click", hideRegisterScreen);
 document.getElementById("cancelButtonLogin").addEventListener("click", hideLoginScreen);
+document.getElementById("registerButton").addEventListener("click", registerUser);
 
 
 
@@ -67,6 +68,52 @@ function hideLoginScreen() {
   }
 
   return false;
+}
+
+function registerUser() {
+  //Input fields
+  var uname = document.getElementById("username").value;
+  var pnumber = document.getElementById("number").value;
+  var emailtxt = document.getElementById("email").value;
+  var passwordtxt =  document.getElementById("password").value;
+  var rePassword = document.getElementById("re_password").value;
+
+  //Color Codes
+  var errorRed = "#cc3c3c";
+  var successGreen = "#60dd60";
+
+  //Button Declaration
+  var button = document.getElementById("registerButton");
+  console.log(uname.length);
+
+  //Front End Validation
+  if(uname.length <= null){
+    alert("Username cannot be blank");
+  }
+  if(pnumber.length <= null){
+    alert("Passord field cannot be blank");
+  }
+  if(emailtxt.length <= 0){
+    alert("Email field cannot be empty")
+  }
+  if(passwordtxt.length <= 0){
+    alert(("password cannot be empty"))
+  }
+  if(passwordtxt = rePassword){
+    alert("Passwords do not match")
+  }
+
+  // createUserWithEmailAndPassword(auth,emailtxt,passwordtxt)
+  //       .then((userCredential) => {
+  //           console.log("Signing In");
+  //           const user = userCredential.user;
+  //           window.location.href = '/dashboard.html';
+  //       })
+  //       .catch((error) => {
+  //           const errorCode = error.code;
+  //           const errorMessage = error.message;
+  //           alert(errorMessage)
+  //       });
 }
 
 
