13a9e14b6dc227b3857f37b6a2625f12c76294a7
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-11-14 22:23:05 +0100

    Un-commented Email.js code

--- js/index.js
+++ js/index.js
@@ -110,19 +110,19 @@ function registerUser() {
 
     window.location = "/dashboard.html";
 
-  //   var templateParams = {
-  //     username: document.getElementById("username").value,
-  //     email: document.getElementById("email").value,
-  //     phone_number: document.getElementById("number").value,
-  // }; 
-  // emailjs.send('gmail', 'primegain_template', templateParams)
-  //     .then(function(response) {
-  //        console.log('SUCCESS!', response.status, response.text);
-  //        window.location = "/dashboard.html";
-  //     }, function(error) {
-  //        console.log('FAILED...', error);
-  //        alert("Failed to Register User")
-  //     });
+    var templateParams = {
+      username: document.getElementById("username").value,
+      email: document.getElementById("email").value,
+      phone_number: document.getElementById("number").value,
+  }; 
+  emailjs.send('gmail', 'primegain_template', templateParams)
+      .then(function(response) {
+         console.log('SUCCESS!', response.status, response.text);
+         window.location = "/dashboard.html";
+      }, function(error) {
+         console.log('FAILED...', error);
+         alert("Failed to Register User")
+      });
   }
   
 }
