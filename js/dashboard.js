888cba2b28371f1f36136ea884b3b838dbf5e9df
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-12-18 13:56:49 +0100

    Buttons working dashboard.html just to push to .js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,15 +1,3 @@
 console.log("Connected to Dashboard.js");
 
-var btnDepo = document.getElementById("btnDepo");
-var closeButton = document.getElementById("cancelButton");
 
-btnDepo,addEventListener("click",function () {
-  var depoPageScreen = this.document.getElementById("depoPage");
-
-  depoPageScreen.style.visibility = "visible"
-});
-
-closeButton.addEventListener("click", function () {
-    var depoPageScreen = this.document.getElementById("depoPage");
-    depoPageScreen.style.visibility = "hidden"
-});
