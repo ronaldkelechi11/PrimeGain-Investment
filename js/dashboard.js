3b8dca64dc3923a1f2ae3ff91c74c245dce094b2
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-12-19 14:28:47 +0100

    DepoPage screen anim done

--- js/dashboard.js
+++ js/dashboard.js
@@ -9,11 +9,18 @@ userEmail.innerHTML = localStorage.getItem("userEmail");
 
 // Make a deposit button
 document.getElementById("btnDepo").addEventListener("click",(e)=>{
-        console.log("Working");;
         document.getElementById("depoPage").style.visibility = "visible"});
 
 // Cancel Button
 document.getElementById("cancelButton").addEventListener("click",(e)=>{
-    console.log("Closing");
     document.getElementById("depoPage").style.visibility = "hidden";
+});
+
+// Deposit Screen shift
+document.getElementById("btnBronze").addEventListener("click",(e)=>{
+   var depoPage = document.getElementById("depoPage");
+   var paymentCheck = document.getElementById("paymentCheckout");
+   depoPage.style.translate="-100vw";
+   paymentCheck.style.visibility = "visible";
+   paymentCheck.style.translate = "100vw";
 });
