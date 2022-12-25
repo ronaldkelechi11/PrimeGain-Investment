52edc422589f937818d35d8417612a42061e4def
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-12-25 21:42:20 +0100

    Started work on the signUp logic

--- js/SignUp.js
+++ js/SignUp.js
@@ -2,6 +2,25 @@ document.getElementById("backToIndex").addEventListener("click",(e)=>{
     window.location = "index.html";
 });
 
+document.getElementById("goToLogin").addEventListener("click", ()=>{
+    window.location = "signIn.html";
+});
+
+// To make sure that the input fields are empty of start
+document.addEventListener("DOMContentLoaded", ()=>{
+    document.getElementById("name").value = null;
+    document.getElementById("email").value = null;
+    document.getElementById("tel").value = null;
+    document.getElementById("password").value = null;
+});
+
+// Sign Up button OnClick
+document.getElementById("signupButton").addEventListener(
+    "click", (e)=>{
+
+    }
+)
+
 
 //   var templateParams = {
 //     username: document.getElementById("username").value,
