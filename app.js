4adebd43b9854af2f8137ad4d3d8427bf2df9eb2
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-12-27 20:02:20 +0100

    Almost forgot

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ app.post("/signUp.html",(req,res)=>{
     var telephone = req.body.telephone;
     var password = req.body.password;
 
-    console.log(email);
     connection.query("INSERT INTO `users`(`id`, `username`, `email`, `telephone`, `password`) VALUES (null,'mmerechi4','mmerechiRonald@gmail.com','09066881954','Password')", (err,result,fields)=>{
         if(err){
             console.log(err);
