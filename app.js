01b964711d7dc954cc51e4a263cf5f49fbbff613
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-12-27 19:18:51 +0100

    Finally first change

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = 3000;
 const app = express();
 
 // Middleware
-app.use(express.static("/public"));
+app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded());
 
@@ -22,23 +22,20 @@ const connection = createConnection({
 });
 
 // To test connection to Mysql Server
-connection.connect(()=>{
-    console.log("Connected to Mysql Server");
-});
+// connection.connect(()=>{
+//     console.log("Connected to Mysql Server");
+// });
 
 // API Routes
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 app.get("signUp",(req,res)=>{
     res.sendFile(__dirname + '/signUp.html');
 });
 
-app.post("/",(req,res)=>{
-
-});
 
 // App port listen function
 app.listen(port,(res)=>{
-    console.log("Server is online");
+    console.log("Server is online on port: " + port);
 });
