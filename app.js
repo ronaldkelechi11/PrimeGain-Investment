e1682f7d81528a3ae023298425a66f1ba9076ada
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2022-12-27 19:29:22 +0100

    Web App working

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = 3000;
 const app = express();
 
 // Middleware
+app.use(express.static("./"))
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded());
