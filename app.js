3407b84d37cdeba4d80895fe736607b37668a852
mani9technologies <96204229+ronaldkelechi11@users.noreply.github.com>
2023-01-19 10:54:50 +0100

    Tried .htaccess

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Import Packages
 const express = require("express")
-const {createConnection} = require("mysql")
+const { createConnection } = require("mysql")
 const bodyParser = require("body-parser")
 const { json } = require("body-parser");
 
@@ -25,7 +25,7 @@ app.use(express.static("./"))
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded());
- 
+
 // Mysql Connection Details
 const connection = createConnection({
     host: "localhost",
@@ -35,52 +35,53 @@ const connection = createConnection({
 });
 
 // To test connection to Mysql Server
-connection.connect((err)=>{
-    if(err) {
+connection.connect((err) => {
+    if (err) {
         console.log("Connection to Mysql DB failed" || "Please put on MySql Server");
     }
-    else{
-       console.log("Connected to Mysql Server"); 
+    else {
+        console.log("Connected to Mysql Server");
     }
 });
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
 // For Refferals
-app.get("/refferal/:username/:id",(req,res)=>{
-   var id = req.params.id;
-   var username = req.params.username;
-   var data = "__"
+app.get("/refferal/:username/:id", (req, res) => {
+    var id = req.params.id;
+    var username = req.params.username;
+    var data = "__"
 
     //To first check if the User actually exists in the refferal Program    
     // Then if not create a new user with refferalNumber 1
-   var searchQuery = "SELECT * FROM `refferals` WHERE `username` =" + " '" + username+"'";
-   var insertNewRefferal = "INSERT INTO `refferals`(`id`, `username`, `refferalAmmout`) VALUES ('" + id + "', '" + username + "', " + " '1');"
+    var searchQuery = "SELECT * FROM `refferals` WHERE `username` =" + " '" + username + "'";
+    var insertNewRefferal = "INSERT INTO `refferals`(`id`, `username`, `refferalAmmout`) VALUES ('" + id + "', '" + username + "', " + " '1');"
+    var updateQuery = ""
 
 
-   connection.query(searchQuery,(err,result,fields)=>{
-    if(err){
-        console.log(err);
-        return true
-    }
-    else if(result = "[]"){
-        connection.query(insertNewRefferal,(err,result,fields)=>{
-            if(err){
-                console.error(err);
-            }
-            else{
-                console.log(result);
-            }
-        })
-    }
+    connection.query(searchQuery, (err, result, fields) => {
+        if (err.errno = 1620) {
+            console.log(err);
+            return
+        }
+        else if (result = "[]") {
+            connection.query(insertNewRefferal, (err, result, fields) => {
+                if (err) {
+                    console.error(err);
+                }
+                else {
+                    console.log(result);
+                }
+            })
+        }
 
-   });
+    });
 
 })
 
 // App port listen function
-app.listen(port,(res)=>{
+app.listen(port, (res) => {
     console.log("Server is online on port: " + port);
 });
--- public/js/SignUp.js
+++ public/js/SignUp.js
@@ -1,56 +1,54 @@
-document.getElementById("backToIndex").addEventListener("click",()=>{
+document.getElementById("backToIndex").addEventListener("click", () => {
     window.location = "/";
 });
 
-document.getElementById("goToSignIn").addEventListener("click", ()=>{
+document.getElementById("goToSignIn").addEventListener("click", () => {
     console.log("Go to Sign In");
     window.location = "signIn.html";
 })
 
 // Sign Up button OnClick
-document.addEventListener("submit", (e)=>{
+document.addEventListener("submit", (e) => {
     localStorage.setItem("name", document.getElementById("name").value)
     localStorage.setItem("email", document.getElementById("email").value)
     localStorage.setItem("tel", document.getElementById("tel").value)
 
     setTimeout(() => {
-       var requestOptions = {
+        var requestOptions = {
             method: 'GET',
             redirect: 'follow'
-          };
-          fetch("http://localhost:4000/signUp.html/apiSignUp", requestOptions)
+        };
+        fetch("http://localhost:4000/signUp.html/apiSignUp", requestOptions)
             .then(response => response.text())
             .then(result => {
-                if(result === "Error"){
+                if (result === "Error") {
                     console.log("Displaying error");
-                    document.getElementById("email").setAttribute("class","error");
-                     alert("Email Has already registered with PrimeGain")
-                        setTimeout(() => {
-                            document.getElementById("email").setAttribute("class",""); 
-                        }, 3000);
+                    document.getElementById("email").setAttribute("class", "error");
+                    alert("Email Has already registered with PrimeGain")
+                    setTimeout(() => {
+                        document.getElementById("email").setAttribute("class", "");
+                    }, 3000);
                     return false;
                 }
-                else if(result === "Succesful"){
-                    window.location = "signIn.html"
+                else if (result === "Succesful") {
+                    var templateParams = {
+                        username: document.getElementById("username").value,
+                        email: document.getElementById("email").value,
+                        phone_number: document.getElementById("tel").value,
+                    };
+                    emailjs.send('gmail', 'primegain_template', templateParams)
+                        .then(function (response) {
+                            console.log('SUCCESS!', response.status, response.text);
+                            window.location = "/login.html";
+                        }, function (error) {
+                            console.log('FAILED...', error);
+                            alert("Failed to Completely Register User")
+                        });
                 }
             })
-            .catch(error => console.log('error', error)); 
+            .catch(error => console.log('error', error));
     }, 1000);
-        
-    
-//   var templateParams = {
-//     username: document.getElementById("username").value,
-//     email: document.getElementById("email").value,
-//     phone_number: document.getElementById("tel").value,
-// }; 
-// emailjs.send('gmail', 'primegain_template', templateParams)
-//     .then(function(response) {
-//        console.log('SUCCESS!', response.status, response.text);
-//        window.location = "/dashboard.html";
-//     }, function(error) {
-//        console.log('FAILED...', error);
-//        alert("Failed to Register User")
-//     });
-    }
+
+}
 );
 
